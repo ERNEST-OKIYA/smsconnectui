e050d4d83bb0c4bb24a077f4b64c3b9c153b0574
Ernest Okiya <okiyaearnest@gmail.com>
2022-08-04 10:17:34 +0300

    build updates

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -7,8 +7,8 @@ const axiosIns = axios.create({
   // You can add your headers here
   // ================================
   // baseURL: '/api/',
-  baseURL: 'https://app.smsconnect.co.ke:2778/api/',
-  // baseURL: 'http://sandbox.mtrackltd.co.ke:2777/api/',
+  // baseURL: 'https://app.smsconnect.co.ke',
+  baseURL: 'https://sandbox.mtrackltd.co.ke:2777/api/',
   // baseURL: 'https://bulkdev.swifttdial.com:2778/api/',
   timeout: 5000,
   // headers: {'X-Custom-Header': 'foobar'}
